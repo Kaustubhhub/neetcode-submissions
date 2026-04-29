334f3ee9ab115c6cb40f104e8d5565cc291b61a1
Kaustubh Kumbhare <90435586+Kaustubhhub@users.noreply.github.com>
2026-04-29 15:00:58 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    bool isAlphanumeric(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+            return true;
+        }
+
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int low = 0, high = s.size()-1;
+cout<<"size : "<<s.size()<<'\n';
+        while(high > low){
+            while(high > low && !isAlphanumeric(s[low])){
+                low++;
+            }
+            while(high > low && !isAlphanumeric(s[high])){
+                high--;
+            }
+
+            if(tolower(s[low]) != tolower(s[high])){
+                cout<<low<<"\n"<<high<<"\n";
+                return false;
+                };
+            high--;
+            low++;
+        }
+
+        return true;
+    }
+};
