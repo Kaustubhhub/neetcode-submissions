335c08bde7d3a278816c2ab86f9af6b29fa70b24
Kaustubh Kumbhare <90435586+Kaustubhhub@users.noreply.github.com>
2026-05-13 03:25:27 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   public:
+    float calculate(int x, int y){
+        return sqrt((x*x)+(y*y));
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> res;
+
+        priority_queue<pair<int, int>> pq;
+
+        for (auto it : points) {
+            if(pq.size()<k){
+                pq.push({it[0], it[1]});
+            }else{
+                float d1 = calculate(it[0],it[1]);
+                if(d1 < calculate(pq.top().first, pq.top().second)){
+                    pq.pop();
+                    pq.push({it[0], it[1]});
+                }
+            }
+        }
+
+        while(!pq.empty()){
+            res.push_back({pq.top().first, pq.top().second});
+            pq.pop();
+        }
+
+        return res;
+    }
+};
